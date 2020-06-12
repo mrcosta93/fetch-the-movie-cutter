8edd8224da32fc15827f0daed3026dd5ca2d6f6a
Marcos Costa <marcosrodcosta@gmail.com>
2020-06-12 18:33:48 -0300

    Uma primeira inserção ao programa

---
+++ parte1.py
@@ -0,0 +1,12 @@
+import os
+
+comeco = input("Digite a hora de começo: ")
+fim = input("Digite a hora de fim: ")
+
+def main():
+    cmd = "ffmpeg -i /home/marcos/Downloads/input.mp4 -ss " + comeco + " -to " + fim + " -c:v copy -c:a copy /home/marcos/Downloads/output.mp4"
+    returned_value = os.system(cmd)  # returns the exit code in unix
+    print('returned value:', returned_value)
+    return returned_value
+
+main()
