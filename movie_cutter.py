1b5e3d4766540cd0a72da73d00ab55d522c0b337
Marcos Costa <marcosrodcosta@gmail.com>
2020-06-23 22:28:35 -0300

    Universalização do código

--- movie_cutter.py
+++ movie_cutter.py
@@ -4,9 +4,11 @@ import os
 comeco = input("Digite a hora de começo: ")
 fim = input("Digite a hora de fim: ")
 nome = input("Entre com o nome do arquivo: ")
+diretorio = os.path.splitext(nome)[0]
+formato = os.path.splitext(nome)[1]
 
 def main():
-    cmd = "ffmpeg -i " + nome + " -ss " + comeco + " -to " + fim + " -c:v copy -c:a copy ~/Downloads/output-fetch.mp4"
+    cmd = "ffmpeg -i " + nome + " -ss " + comeco + " -to " + fim + " -c:v copy -c:a copy " + diretorio + "-cortado" + formato
     returned_value = os.system(cmd)  # returns the exit code in unix
     print('returned value:', returned_value)
     return returned_value
